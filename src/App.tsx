3ed95ba86839082cba633d0264b67f9848b982a8
rafaelteruyuki <rafaelteruyuki08@gmail.com>
2022-01-04 14:13:49 -0300

    Utiliza componente formulario

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.scss';
+import FormularioCadastro from './components/formulario-cadastro';
 
 function App() {
   return (
-    <h1>Formulário de Cadastro</h1>
+    <>
+      <h1>Formulário de Cadastro</h1>
+      <FormularioCadastro />
+    </>
   );
 }
 
